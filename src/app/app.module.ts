0097c64d56c883e3c46fba7d766eec4406a892cc
jinalshah999 <jinalshah999@gmail.com>
2019-12-13 21:28:13 +0530

    done

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { YouTubePlayerModule } from "@angular/youtube-player";
+import { GoogleMapsModule } from "@angular/google-maps";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClipboardModule } from "@angular/cdk/clipboard";
+
+import { AppComponent } from './app.component';
+import { VideoComponent } from './video/video.component';
+import { MapComponent } from './map/map.component';
+import { CbDemoComponent } from './cb-demo/cb-demo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VideoComponent,
+    MapComponent,
+    CbDemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    YouTubePlayerModule,
+    GoogleMapsModule,
+    ClipboardModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
